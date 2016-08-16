36eff9b99b1dbbf3035e3a404f9d57a6b202317c
KC YAP <takeshisean@hotmail.com>
2016-08-17 06:29:07 +0800

    Added MySortedList

--- MyCSharpTutorial/MyCSharpTutorial/Form1.cs
+++ MyCSharpTutorial/MyCSharpTutorial/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyCSharpTutorial.General;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,24 @@ namespace MyCSharpTutorial
         public Form1()
         {
             InitializeComponent();
+            ExecMySortedList();
+        }
+
+        public void ExecMySortedList()
+        {
+            MySortedList list = new MySortedList();
+            list.Add(13);
+            list.Add(81);
+            list.Add(25);
+            list.Add(9);
+            list.Add(33);
+            list.Add(46);
+            list.Add(1);
+            foreach (int v in list.Items)
+            {
+                Console.WriteLine(v);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ MyCSharpTutorial/MyCSharpTutorial/General/MySortedList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpTutorial.General
+{
+    class MySortedList
+    {
+        int[] items = new int[0];
+        public int[] Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+
+        public void Add(int value)
+        {
+            int index = Array.BinarySearch(items, value);
+            if (index < 0)
+                index = ~index;
+
+            //Increase Items Array Size by 1
+            int lastIndex = items.Length;
+            int capacity = items.Length + 1;
+            int[] dupArray = new int[capacity];
+            Array.Copy(items, dupArray, items.Length);
+            items = dupArray;
+
+            //Adjusting elements to insert element in its right index
+            if (index < lastIndex)
+            {
+                Array.Copy(items, index, items, index + 1, lastIndex - index);
+            }
+            items[index] = value;
+        }
+    }
+}
